a64d74e33971b22515bb7df72ff6bee5d0241701
TheFoxy41 <thefoxy41@gmail.com>
2020-04-13 22:19:34 +0200

    Some updates

--- src/main/java/fr/thefoxy41/dataManager/redis/RedisAccess.java
+++ src/main/java/fr/thefoxy41/dataManager/redis/RedisAccess.java
@@ -1,6 +1,5 @@
 package fr.thefoxy41.dataManager.redis;
 
-import fr.thefoxy41.dataManager.auth.Credentials;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
@@ -18,15 +17,14 @@ public class RedisAccess {
     public RedissonClient setupRedisson(RedisCredentials credentials) {
         Config config = new Config();
         config.setCodec(new JsonJacksonCodec());
-        config.setThreads(12);
-        config.setNettyThreads(12);
+        config.setThreads(16);
+        config.setNettyThreads(32);
         config.useSingleServer()
                 .setAddress(credentials.toUri())
                 .setPassword(credentials.getPassword())
                 .setDatabase(credentials.getDatabaseId())
                 .setRetryInterval(1000)
                 .setRetryAttempts(5)
-                .setConnectionMinimumIdleSize(Credentials.DEFAULT_POOL_SIZE)
                 .setConnectionPoolSize(credentials.getPoolSize())
                 .setClientName(credentials.getClientName());
 
